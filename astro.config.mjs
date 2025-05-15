bd118a53b56e8dbee1b8909964e8eb210eade9a1
Anatoly Zhuravlev <newstreetpunk@yandex.ru>
2025-05-15 17:31:16 +0500

    start settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,7 +12,7 @@ import react from '@astrojs/react';
 export default defineConfig({
 	integrations: [
 		tailwind({
-			configFile: './tailwind.config.js'
+			configFile: './tailwind.kaiyi.js'
 		}),
 		sitemap({
 			filter: (page) => !page.endsWith('telegram-bot/') && !page.endsWith('redirect/')
@@ -54,6 +54,6 @@ export default defineConfig({
 			},
 		},
 	},
-	site: 'https://site.com',
+	site: 'https://sales-autoholding-turgenevskiy.alexsab.ru',
 	base: "/"
 });
